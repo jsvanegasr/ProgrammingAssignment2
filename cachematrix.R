4400cc8ffe3214e1a042e75fae73755bcf92adba
jsvanegasr <jsvanegasr@gmail.com>
2014-04-29 13:55:52 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Scrip para alamcenar y calcular la inversa de una matriz##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##Creacion de objeto matrix##
+        inverse <- NULL  ##Inicializacion de la matriz inversa##
+        set <- function(y) { ##Funcion para obtener los valores de la matriz##
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x ##Retorno de la matriz##
+        setinverse <- function(m) inverse <<- m  ##Se obtiene la matriz inversa##
+        getinverse <- function() inverse ##Retorna la inversa de la matriz##
+        list(set = set, get = get,  ##Lista de Funciones##
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ##Funcion que calcula y retorna la inversa de la matriz x##
+        inv <- x$getinverse()
+        if(!is.null(inv)) {  ##Primero se obtiene la inversa de la matriz##
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get() ##Si la inversa ya esta almacenada obtiene x ##
+        inv <- solve(data) ##Calcula la inversa de la matriz x##
+        x$setinverse(inv) ##Se obtiene la inversa del objeto x##
+        inv ##Retorna la inversa##
 }
